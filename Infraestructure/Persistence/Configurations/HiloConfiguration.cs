dc4207c118e366dbe115d06f4778ffb46b01a50d
Franco <ff>
2025-01-16 20:55:52 -0300

    agregado configuracion de hilo

---
+++ Infraestructure/Persistence/Configurations/HiloConfiguration.cs
@@ -0,0 +1,26 @@
+using Domain.Hilos.Models;
+using Domain.Usuarios.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infraestructure.Persistence.Configurations
+{
+    internal sealed class HiloConfiguration : IEntityTypeConfiguration<Hilo>
+    {
+        public void Configure(EntityTypeBuilder<Hilo> builder)
+        {
+            builder.HasKey(h => h.Id);
+            builder.Property(h => h.Id).HasConversion(id => id.Value, value => new(value)).HasColumnName("id");
+        
+            builder.OwnsOne(h => h.Configuracion, conf =>
+                {
+                    conf.Property(c => c.Dados).HasColumnName("Dados");
+                    conf.Property(c => c.IdUnicoActivado).HasColumnName("IdUnicoActivado");
+                }
+            );
+
+            builder.HasOne<Usuario>().WithMany().HasForeignKey(h=> h.AutorId);
+            
+        }    
+     }    
+}
