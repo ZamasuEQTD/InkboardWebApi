ee9d27fb2d88b577064749bea4133f97055677e7
Franco <ff>
2025-01-16 16:01:07 -0300

    agregado swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -18,19 +18,20 @@ builder.Services.AddPersistence(builder.Configuration);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-}
 
-app.MapGet("/", (UserManager<Usuario> manager) => {
-    manager.FindByIdAsync(Guid.NewGuid().ToString());
-    
-    return "Hello World!";
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
